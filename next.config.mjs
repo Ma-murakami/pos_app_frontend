74f232c11be6b8b9ffff6835ed48bd753e0ac3a4
Ma-murakami <166308764+Ma-murakami@users.noreply.github.com>
2024-10-27 21:01:31 +0900

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // スタンドアロンモードを有効化して、必要最低限のファイルのみをデプロイ
+    output: 'standalone',
+
+    // 環境変数の設定
+    env: {
+        NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL, // Azureで設定されたAPI URLを使用
+    },
+};
+
+export default nextConfig;
